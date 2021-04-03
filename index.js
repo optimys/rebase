9f4bb4b876ce04114f94673911d609239a8d5add
Alex Kalshnikov <lagyerto@gmail.com>
2021-04-03 17:11:38 +0300

    added console.log of full name

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const user = {
     get fullName(){
         return `${this.name} ${this.lastName}`
     },
-    sayHello(name){
+    sayHello({name='Piter'}={}){
         return `Hello ${name}, nice to meet you, my name is ${this.fullName}`
     }
 }
 
+const subUser = Object.create(user)
+subUser.name = 'Piter'
+
 console.log(user.fullName)
+console.log(user.sayHello(subUser))
